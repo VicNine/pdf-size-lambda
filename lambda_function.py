5f05fed15101b760695c4ea297e1209907020032
Victor <viccc.z.wang@gmail.com>
2023-10-24 02:04:02 +1100

    update lambda function and buildspec

--- lambda_function.py
+++ lambda_function.py
@@ -1,6 +1,22 @@
+import base64
+
 def lambda_handler(event, context):
-    print("test")
+    # get base64 encoded file content from event
+    base64_file_content = event['body']
+    if not base64_file_content:
+        return {
+            'statusCode': 400,
+            'body': 'No file provided'
+        }
+    
+    # decode base64 encoded file content
+    file_content = base64.b64decode(base64_file_content)
+    
+    # calculate file size in megabytes
+    file_size_in_bytes = len(file_content)
+    file_size_in_megabytes = file_size_in_bytes / (1024 * 1024)
+    
     return {
         'statusCode': 200,
-        'body': "test"
+        'body': f'File size: {file_size_in_megabytes:.2f} MB'
     }
