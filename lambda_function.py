6dc4350ed6837cfed4cfde99b46b41011d486273
Victor <viccc.z.wang@gmail.com>
2023-10-24 10:39:27 +1100

    update build spec, added store s3

--- lambda_function.py
+++ lambda_function.py
@@ -1,8 +1,10 @@
 import base64
+import boto3
+import os
 
 def lambda_handler(event, context):
     # get base64 encoded file content from event
-    base64_file_content = event['body']
+    base64_file_content = event['body']['file_content_base64']
     if not base64_file_content:
         return {
             'statusCode': 400,
@@ -15,6 +17,21 @@ def lambda_handler(event, context):
     # calculate file size in megabytes
     file_size_in_bytes = len(file_content)
     file_size_in_megabytes = file_size_in_bytes / (1024 * 1024)
+
+    # store file in S3 bucket
+    bucket_name = os.environ['BUCKET_NAME']
+    s3 = boto3.client('s3')
+    file_name = event['body']['file_name']
+
+    try:
+        s3.put_object(Bucket=bucket_name, Key=file_name, Body=file_content, ContentType='application/pdf')
+        print(f'Successfully uploaded {file_name} to {bucket_name}.')
+    except Exception as e:
+        print(f'Error uploading {file_name} to {bucket_name}: {e}')
+        return {
+            'statusCode': 500,
+            'body': 'File upload failed'
+        }
     
     return {
         'statusCode': 200,
